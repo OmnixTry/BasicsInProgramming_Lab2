4f24fea1a93b3f2f70b9950b06e098b21981603d
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-03-04 21:52:42 +0200

    main.cpp returned to normal

--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -2,18 +2,18 @@
 #include <string>
 #include <fstream>
 #include "ReadFile.h"
-#include "Calculated_point.h"
+#include "SplitComplite.h"
+#include <vector>
+#include <Windows.h>
 using namespace std;
 
 int main() {
-
-int count_country;
-	string *arr = ReadFile("eurovision.csv",count_country);
-	string asd = Calculated_point(arr,count_country);
-
-
-
-
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int count_country = 0;
+	vector<string> arr = ReadFile("eurovision.csv", count_country);
+	vector<string> *array = new vector<string>[count_country];
+	array = SplitComplite(arr, count_country);
 	system("pause");
 	return 0;
 }
