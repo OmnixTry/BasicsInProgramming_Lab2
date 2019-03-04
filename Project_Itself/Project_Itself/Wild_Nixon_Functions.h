e0870800709c54241b525605ef105538aa30b467
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-03-04 21:32:30 +0200

    Wild_nixon_Functions.h was finished

--- Project_Itself/Project_Itself/Wild_Nixon_Functions.h
+++ Project_Itself/Project_Itself/Wild_Nixon_Functions.h
@@ -1,1 +1,6 @@
 #pragma once
+using namespace std;
+
+void summing_up(int **points, int *sums, int size);
+void winners(int *sums, string *countries, int size);
+void contest_results(int **points, string *countries);
--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -2,18 +2,18 @@
 #include <string>
 #include <fstream>
 #include "ReadFile.h"
-#include "SplitComplite.h"
-#include <vector>
-#include <Windows.h>
+#include "Calculated_point.h"
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int count_country =0;
-	vector<string> arr = ReadFile("eurovision.csv",count_country);
-	vector<string> *array = new vector<string>[count_country];
-	array = SplitComplite(arr,count_country);
+
+int count_country;
+	string *arr = ReadFile("eurovision.csv",count_country);
+	string asd = Calculated_point(arr,count_country);
+
+
+
+
 	system("pause");
 	return 0;
 }
