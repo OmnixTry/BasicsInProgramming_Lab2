c3a7d20bd13d69a2333405533c6595d627cac2b0
Siusarna <charodeyrap@gmail.com>
2019-03-04 10:36:45 +0200

    Create function Calculated_point

--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "structure.h"
-#include "Wild_Nixon_Functions.h"
+#include <string>
+#include <fstream>
+#include "ReadFile.h"
+#include "Calculated_point.h"
 using namespace std;
 
 int main() {
-
-
-
-
-
+	int count_country;
+	string *arr = ReadFile("eurovision.csv",count_country);
+	string asd = Calculated_point(arr,count_country);
 
 	system("pause");
 	return 0;
